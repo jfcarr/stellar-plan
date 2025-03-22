1f4543468e429a71d706e52f72004dd9fac77af5
Jim Carr <jim.carr@devtoprd.com>
2025-03-22 13:38:46 -0400

    added bright stars, parent constellations

--- stellarplan/data/deep_sky_objects.py
+++ stellarplan/data/deep_sky_objects.py
@@ -8,9 +8,10 @@ class DeepSkyObject:
     Information about a deep sky object.
     """
 
-    def __init__(self, name, description, type, magnitude):
+    def __init__(self, name, description, constellation, type, magnitude):
         self.name = name
         self.description = description
+        self.constellation = constellation
         self.type = type
         self.magnitude = magnitude
 
@@ -24,32 +25,70 @@ def load_dso():
     # bright stars
     deep_sky_objects.extend(
         [
-            DeepSkyObject("Aldebaran", "Aldebaran", "double star, variable star", 0.85),
-            DeepSkyObject("Arcturus", "Arcturus", "double star", 0.15),
             DeepSkyObject(
-                "Betelgeuse", "Betelgeuse", "double star, pulsating variable star", 0.45
+                "Aldebaran", "Aldebaran", "Taurus", "double star, variable star", 0.86
             ),
-            DeepSkyObject("Capella", "Capella", "double star", 0.05),
+            DeepSkyObject(
+                "Altair",
+                "Altair",
+                "Aquila",
+                "double star, pulsating variable star",
+                0.76,
+            ),
+            DeepSkyObject(
+                "Antares",
+                "Antares",
+                "Scorpius",
+                "double star, pulsating variable star",
+                0.6,
+            ),
+            DeepSkyObject("Arcturus", "Arcturus", "Bootes", "double star", -0.05),
+            DeepSkyObject(
+                "Betelgeuse",
+                "Betelgeuse",
+                "Orion",
+                "double star, pulsating variable star",
+                0.50,
+            ),
+            DeepSkyObject("Capella", "Capella", "Auriga", "double star", 0.08),
+            DeepSkyObject("Canopus", "Canopus", "Carina", "double star", -0.74),
             DeepSkyObject(
                 "Polaris",
                 "Polaris (North Star)",
+                "Ursa Minor",
                 "double star, pulsating variable star",
-                1.95,
+                1.98,
             ),
-            DeepSkyObject("Sirius", "Sirius", "double star", -1.45),
+            DeepSkyObject("Procyon", "Procyon", "Canis Minor", "double star", 0.34),
+            DeepSkyObject(
+                "Rigel", "Rigel", "Orion", "double star, pulsating variable star", 0.13
+            ),
+            DeepSkyObject("Sirius", "Sirius", "Canis Major", "double star", -1.46),
+            DeepSkyObject(
+                "Spica", "Spica", "Virgo", "double star, variable star", 0.97
+            ),
+            DeepSkyObject(
+                "Vega", "Vega", "Lyra", "double star, pulsating variable star", 0.026
+            ),
         ]
     )
 
     # other objects
     deep_sky_objects.extend(
         [
-            DeepSkyObject("M33", "Triangulum Galaxy", "galaxy", 5.70),
-            DeepSkyObject("M31", "Andromeda Galaxy", "galaxy", 3.40),
-            DeepSkyObject("M42", "Great Orion Nebula", "nebula", 4.00),
+            DeepSkyObject("M33", "Triangulum Galaxy", "Triangulum", "galaxy", 5.70),
+            DeepSkyObject("M31", "Andromeda Galaxy", "Andromeda", "galaxy", 3.40),
+            DeepSkyObject("M42", "Great Orion Nebula", "Orion", "nebula", 4.00),
             DeepSkyObject(
-                "M45", "Pleiades - Seven Sisters", "cluster (with nebulosity)", 1.20
+                "M45",
+                "Pleiades - Seven Sisters",
+                "Taurus",
+                "cluster (with nebulosity)",
+                1.20,
             ),
-            DeepSkyObject("M3", "NGC 5272", "globular star cluster", 3.40),
+            DeepSkyObject(
+                "M3", "NGC 5272", "Canes Venatici", "globular star cluster", 3.40
+            ),
         ]
     )
 
